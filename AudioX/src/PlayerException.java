f5bae6a70d63dbfc45a8210937ee61aed0984658
nur <snurislam97@mail.ru>
2016-05-22 18:07:17 +0300

    Fourth commit

---
+++ AudioX/src/PlayerException.java
@@ -0,0 +1,18 @@
+
+/**
+ *
+ * @author Alexander Ferenets (Istamendil) <ist.kazan@gmail.com>
+ */
+public class PlayerException extends RuntimeException {
+
+  public PlayerException() {
+  }
+
+  public PlayerException(String msg) {
+    super(msg);
+  }
+  
+  public PlayerException(Throwable ex){
+    super(ex);
+  }
+}
